29d42e8e37b0f09e5ad8b1fcd3adf3b2ccf49ce1
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-02-18 11:58:34 +0600

    click and scroll down js created

---
+++ script/index.js
@@ -0,0 +1,10 @@
+
+//click to scroll
+
+function buyTicketScroll(){
+    let ticketSection = document.getElementById('scroll-div');
+    ticketSection.scrollIntoView({behavior: 'smooth'});
+}
+
+let selectedSeats = 0;
+let totalAmount = 0;
