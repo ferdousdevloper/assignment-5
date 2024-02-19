f9e49e0a3071c5a8788e8957e8799e943e831a25
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-02-19 13:16:57 +0600

    scroll to div compleate

--- script/index.js
+++ script/index.js
@@ -5,4 +5,8 @@ function buyTicketScroll(){
     let ticketSection = document.getElementById('scroll-div');
     ticketSection.scrollIntoView({behavior: 'smooth'});
 }
+function buyTicket(){
+    let ticketSection = document.getElementById('buy-ticket');
+    ticketSection.scrollIntoView({behavior: 'smooth'});
+}
 
